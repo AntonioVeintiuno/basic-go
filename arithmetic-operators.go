5e2c4698d57d4e75bce5530aca1af98e2a5d7d41
Antonio Garcia <moy_357@hotmail.com>
2018-08-06 16:23:32 -0500

    Adding simple operations file.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

---
+++ arithmetic-operators.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+  var numero1 int = 10;
+  var numero2 int = 6;
+
+  //Suma
+  fmt.Print("La suma es:")
+  fmt.Println(numero1 + numero2)
+
+  //Resta
+  fmt.Print("La resta es:")
+  fmt.Println(numero1 - numero2)
+
+  //Multiplicaión
+  fmt.Print("La multiplicación es:")
+  fmt.Println(numero1 * numero2)
+
+  //División
+  fmt.Print("La división es:")
+  fmt.Println(numero1 / numero2)
+}
