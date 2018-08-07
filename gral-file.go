ca332c1b3564818eff67b81d12bc359777238084
Antonio Garcia <moy_357@hotmail.com>
2018-08-06 20:05:11 -0500

    Adding clousure example on GO (nested functions).
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- gral-file.go
+++ gral-file.go
@@ -4,8 +4,23 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println(returnText());
+  // fmt.Println(returnText());
+  fmt.Print("Order 1 -->");
+  fmt.Println(hats("$", 45));
+
+  fmt.Println("~~~~~~~~~~~~~~~~");
+
+  fmt.Print("Order 2 -->");
+  fmt.Println(hats("$", 24 ));
 }
+
+func hats(coin string, order float32) (string, string, float32) {
+  price := func() float32 {
+    return order * 7
+  }
+  return "Hat price is: ", coin, price()
+}
+
 // this fuction return two results
 func returnText() (string, int) {
   dataone := "Moee"
