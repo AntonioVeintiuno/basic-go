ff6771af87534fd761920dffc8c2c78ca2dd7170
Antonio Garcia <moy_357@hotmail.com>
2018-08-07 10:35:35 -0500

    Adding example to use loop (for is the uniq loop use on GO).
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- gral-file.go
+++ gral-file.go
@@ -5,13 +5,20 @@ import "fmt"
 
 func main() {
   // fmt.Println(returnText());
-  fmt.Print("Order 1 -->");
-  fmt.Println(hats("$", 45));
+  // fmt.Print("Order 1 -->");
+  // fmt.Println(hats("$", 45));
 
-  fmt.Println("~~~~~~~~~~~~~~~~");
+  // fmt.Println("~~~~~~~~~~~~~~~~");
 
-  fmt.Print("Order 2 -->");
-  fmt.Println(hats("$", 24 ));
+  // fmt.Print("Order 2 -->");
+  // fmt.Println(hats("$", 24 ));
+  pants("red", "large", "nike", "no bags" )
+}
+
+func pants(descriptions ...string) {
+  for _, description := range descriptions {
+    fmt.Println(description)
+  }
 }
 
 func hats(coin string, order float32) (string, string, float32) {
